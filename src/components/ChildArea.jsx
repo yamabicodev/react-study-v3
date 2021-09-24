f52ddfaa06f3fce66c35044512a461d3e20367e9
yamabico_dev <38546765+yamabicodev@users.noreply.github.com>
2021-09-24 09:19:09 +0900

    Initial commit

---
+++ src/components/ChildArea.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+export const ChildArea = (props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
