e5b805b7ca7f05b748d05e2fd7f0e74187e28f8e
yamabico_dev <38546765+yamabicodev@users.noreply.github.com>
2021-09-25 09:19:29 +0900

    :+1:update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./components/ChildArea";
 
 export default function App() {
+  console.log("App is re-rendering!!");
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
   return (
     <div className="App">
       <input type="text" onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import { memo } from "react";
 
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
-export const ChildArea = (props) => {
-  const { open } = props;
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildArea is re-rendering!!");
+  const testdata = [...Array(2000).keys()];
+  testdata.forEach(() => {
+    console.log("testdata");
+  });
+
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
